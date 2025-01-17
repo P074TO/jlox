64c0e2f700bd9243aa59f93d9c4b9aa3a254543e
p074to <jrrobsaluday26@gmail.com>
2025-01-17 18:44:54 +0800

    refactor: start over, init framework class

--- Lox.java
+++ Lox.java
@@ -1,4 +1,4 @@
-package com.craftinginterpreters.lox;
+package com.p074to.lox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,7 +9,6 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -46,16 +45,6 @@ public class Lox {
     for (Token token : tokens) {
       System.out.println(token);
     }
-  }
-  static void error(int line, String message) {
-    report(line, "", message);
-  }
-
-  private static void report(int line, String where,
-                             String message) {
-    System.err.println(
-        "[line " + line + "] Error" + where + ": " + message);
-    hadError = true;
   }
 
 }
